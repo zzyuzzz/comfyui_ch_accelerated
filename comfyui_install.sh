ac1a7131dd03847bd7654b25714a21a488ed0907
395143360 <395143360@qq.com>
2024-10-31 14:28:30 +0800

    10311428

--- comfyui_install.sh
+++ comfyui_install.sh
@@ -34,19 +34,19 @@ pip install -r $cdir/ComfyUI/custom_nodes/ComfyUI-Impact-Pack/requirements.txt
 pip install -r $cdir/ComfyUI/custom_nodes/ComfyUI-GGUF/requirements.txt
 
 cd $cdir/ComfyUI/models/unet
-wget -c https://hf-mirror.com/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q2_K.gguf?download=true -O flux1-dev-Q2_K.gguf
+wget -c https://hf-mirror.com/city96/FLUX.1-dev-gguf/resolve/main/flux1-dev-Q2_K.gguf?download=true -O flux1-dev-Q2_K.gguf &
 
 cd $cdir/ComfyUI/models/clip
-wget -c https://hf-mirror.com/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors?download=true -O clip_l.safetensors
-wget -c https://hf-mirror.com/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true -O t5xxl_fp8_e4m3fn.safetensors
+wget -c https://hf-mirror.com/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors?download=true -O clip_l.safetensors &
+wget -c https://hf-mirror.com/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true -O t5xxl_fp8_e4m3fn.safetensors &
 
 cd $cdir/ComfyUI/models/vae
-wget -c https://hf-mirror.com/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors?download=true -O ae.safetensors
+wget -c https://hf-mirror.com/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors?download=true -O ae.safetensors &
 
 cd $cdir/ComfyUI/models/clip_vision
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors -O CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/image_encoder/model.safetensors -O CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors
-wget -c https://hf-mirror.com/Kwai-Kolors/Kolors-IP-Adapter-Plus/resolve/main/image_encoder/pytorch_model.bin -O clip-vit-large-patch14-336.bin
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors -O CLIP-ViT-H-14-laion2B-s32B-b79K.safetensors &
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/image_encoder/model.safetensors -O CLIP-ViT-bigG-14-laion2B-39B-b160k.safetensors &
+wget -c https://hf-mirror.com/Kwai-Kolors/Kolors-IP-Adapter-Plus/resolve/main/image_encoder/pytorch_model.bin -O clip-vit-large-patch14-336.bin &
 
 if [ ! -d "$cdir/ComfyUI/models/ipadapter" ];then 
     mkdir $cdir/ComfyUI/models/ipadapter
@@ -54,11 +54,12 @@ else
     echo "$cdir/ComfyUI/models/ipadapter existed!"
 fi
 cd $cdir/ComfyUI/models/ipadapter
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl_vit-h.safetensors
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus-face_sdxl_vit-h.safetensors
-wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl.safetensors
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl_vit-h.safetensors &
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors &
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus-face_sdxl_vit-h.safetensors &
+wget -c https://hf-mirror.com/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl.safetensors &
 
+wait
 # cd $cdir/ComfyUI/
 # python main.py
 
